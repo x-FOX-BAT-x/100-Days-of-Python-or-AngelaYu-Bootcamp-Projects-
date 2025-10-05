3c5bd30540849633b83e67545ef6c7a3bd672215
shayan azambakhtiar <shayan73160@gmail.com>
2025-10-04 21:20:19 -0500

    Added Day-001 directory with Python placeholder for bootcamp tracking

---
+++ Day-001/Day-1_Band_Name_Generator.py
@@ -0,0 +1,7 @@
+# First ever program made by shayan. Look out for me world
+print("\nHello and welcome to the Band Name Generator!!!!\n")
+CITY=input(" What city you were born in?\n")
+PET=input(" What is your pets name?\n")
+print("Your band name is:\n"+CITY+" "+PET+"!")
+NBN="CITY"+"PET"
+print("Your new band name has:\n"+str(len(NBN))+" letters!")
