890252e2c8020612c3e0874cec189ce19255dc17
shayan azambakhtiar <shayan73160@gmail.com>
2025-10-07 20:18:41 -0500

    Clean up renamed BMI calculator file

--- Day-002/simple BMI calculator.py
+++
@@ -1,9 +0,0 @@
-#simple BMI calculator
-print("Welcome to the BMI calculator. Please answer the below prompts:\n\n")
-weight=int(input("Please input your weight in KG:\n\t"))
-height=float(input("Please input your height in m:\n\t"))
-BMI= weight/(height**2)
-print(round(BMI,3))
-
-
-
